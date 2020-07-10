6691c2340597e9809f80abda362a6f34094b4327
Hannah Chang <35381777+ch0mper@users.noreply.github.com>
2020-07-10 07:44:19 -0500

    :tada: first commit: firebase deploy and some context

---
+++ src/components/AddApplicant.js
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react'
+import { ThemeContext } from '../App'
+
+const AddApplicant = () => {
+  const applicants = useContext(ThemeContext)
+
+  return (
+    <div>
+      <div>applicants</div>
+      <form>
+        <input />
+        <button
+          onClick={() =>
+            console.log('clicked')
+            // add applicant to context
+          }
+        >
+          add
+        </button>
+      </form>
+      {applicants.map((applicant) => (
+        <div key={applicant.name} style={{ border: '1px solid black' }}>
+          {applicant.name}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default AddApplicant
